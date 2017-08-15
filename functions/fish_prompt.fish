ad26e74ee8aaf157ef79180ff1b22d9614926844
lethalpaga <lethalpaga@gmail.com>
2017-08-16 11:51:34 +1200

    Add github ci status and aws status

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -11,9 +11,19 @@ function __theme_git_prompt
 	if test $status -eq 0
 		set -l raw_ref (command git symbolic-ref HEAD 2> /dev/null; or command git rev-parse --short HEAD 2> /dev/null)
 		set -l ref (echo -n $raw_ref | sed -e 's|refs/heads/||')
-		set -l dirty (test (count $git_status) -gt 0; and echo -n "*")
-		echo -n " $argv[1]$ref$dirty$argv[2]"
+		set -l dirty (test (count $git_status) -gt 0; and echo -n "●")
+    set -l cistatus ""
+    if [ ! -z "$FISH_GITHUB_STATUS" ]
+      set cistatus (__theme_github_status)
+    end
+		echo -n " $argv[1]$ref $dirty$cistatus$argv[2]"
 	end
+end
+
+function __theme_aws_env
+  if [ ! -z "$AWS_VAULT" ]
+    echo -n " ‹"$AWS_VAULT"›"
+  end
 end
 
 function __theme_terraform_env
@@ -44,6 +54,27 @@ function __theme_hashiline
   hashiline
 end
 
+function __theme_github_status
+  # If hub is installed
+  if type -q hub
+    set -l res (git ci-status 2>&1)
+
+    switch "$res"
+    case "success"
+      set_color green
+      echo -n "●"
+    case "pending"
+      set_color orange
+      echo -n "●"
+    case "Aborted: the origin remote doesn't point to a GitHub repository."
+      echo -n ""
+    case "*"
+      set_color red
+      echo -n "●"
+    end
+  end
+end
+
 function fish_prompt
 	echo -n (hostname)
 	set_color --bold blue
@@ -52,7 +83,9 @@ function fish_prompt
 	set_color green
 	echo -n (__theme_short_pwd)
 	set_color yellow
-	__theme_git_prompt "‹" "›"
+	__theme_git_prompt "" ""
+  set_color blue
+  __theme_aws_env
   set_color blue
   __theme_terraform_env
   set_color purple
